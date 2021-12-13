826a4cb9407a9616901f9039d30f7dbf2b363be8
Ana Clara Mendes Bull <89466535+anaclarabull@users.noreply.github.com>
2021-12-13 15:23:43 -0300

    navigation

--- App.js
+++ App.js
@@ -2,138 +2,27 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, Platform, KeyboardAvoidingView, Keyboard, TouchableWithoutFeedback} from 'react-native';
 import { FontAwesome5} from '@expo/vector-icons';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+import Feed from './src/screens/Feed/Feed';
+import Login from './src/screens/Login/Login';
+
+const Stack = createStackNavigator();
+
+function MyStack() {
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-    >
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      
-        <View style={styles.container}>
-          <StatusBar backgroundColor="#FFF" transLucent={false} />
-          
-          <Image 
-            source={require('./src/assets/logo.png')}
-            style={styles.logo}
-          />
-
-          <TextInput
-            placeholder="Celular, username ou email"
-            style={styles.input}
-          />
-          <TextInput
-            placeholder="Sua senha"
-            style={styles.input}
-          />
-
-          <View style={styles.forgotContainer}>
-            <TouchableOpacity>
-              <Text style={styles.forgotText}>Esqueceu a senha?</Text>
-            </TouchableOpacity>
-          </View>
-
-          <TouchableOpacity style={styles.loginButton}>
-            <Text style={styles.loginText}>Acessar</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.facebookContainer}>
-            <FontAwesome5 name="facebook" size={25} color= "#399fff"/>
-            <Text style={styles.facebookText}>Continue como Marlon Brendon</Text>
-          </TouchableOpacity>
-
-          <View style={styles.divisor}>
-            <View style={styles.divisorLine}></View>
-            <Text style={{marginHorizontal: '3%' }}>OU</Text>
-            <View style={styles.divisorLine}></View>
-          </View>
-
-          <View style={styles.signUpContainer}>
-            <Text style={styles.signUpText}>Não possui uma conta?</Text>
-            <TouchableOpacity>
-              <Text style={styles.signUpButton}>Cadastre-se</Text>
-            </TouchableOpacity>
-          </View>
-
-        </View>
-      </TouchableWithoutFeedback>
-    </KeyboardAvoidingView>
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Feed" component={Feed} />
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container:{
-    backgroundColor: '#FFF',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo:{
-    marginTop: Platform.OS === 'android' ? '13%' : '20%',
-    marginBottom: Platform.OS === 'android' ? '13%' : '15%',
-  },
-  input:{
-    width: '90%',
-    height: 42,
-    backgroundColor: '#F4F3F3',
-    marginBottom: 20,
-    padding: 8,
-    borderRadius: 5,
-    borderWidth: 1,
-    borderColor: '#E0E0E0',
-  },
-  forgotContainer:{
-    width: '90%',
-    alignItems: 'flex-end'
-  },
-  forgotText:{
-    color: '#399fff',
-  },
-  loginButton:{
-    marginTop: '5%',
-    backgroundColor: '#399fff',
-    width: '90%',
-    height: 42,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 5,
-  },
-  loginText: {
-    color: '#fff',
-    fontSize: 17,
-  },
-  facebookContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginTop: '15%',
-  },
-  facebookText:{
-    color: '#399fff',
-    paddingLeft: 8,
-    fontSize: 15,
-  },
-  divisor:{
-    marginTop: '10%',
-    flexDirection: 'row',
-    width: '90%',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  divisorLine:{
-    width: '45%',
-    height: 2,
-    backgroundColor: '#EFEDED',
-    borderRadius: 5,
-  },
-  signUpContainer:{
-    flexDirection: 'row',
-    marginTop: '10%',
-  },
-  signUpText:{
-    color: '#c4c4c4',
-    paddingRight: 5,
-  },
-  signUpButton:{
-    color: '#399fff',
-    fontWeight: 'bold',
-  }
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
---
+++ src/screens/Login/Login.js
@@ -0,0 +1,153 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity, Platform, KeyboardAvoidingView, Keyboard, TouchableWithoutFeedback} from 'react-native';
+import { FontAwesome5} from '@expo/vector-icons';
+
+export default function Login({navigation}) {
+
+    const acessar = () => {
+        navigation.navigate("Feed")
+        navigation.reset({
+            index: 0,
+            routes: [{name: "Feed"}]
+        })
+    }
+
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      
+        <View style={styles.container}>
+          <StatusBar backgroundColor="#FFF" transLucent={false} />
+          
+          <Image 
+            source={require('../../assets/logo.png')}
+            style={styles.logo}
+          />
+
+          <TextInput
+            placeholder="Celular, username ou email"
+            style={styles.input}
+          />
+          <TextInput
+            placeholder="Sua senha"
+            style={styles.input}
+          />
+
+          <View style={styles.forgotContainer}>
+            <TouchableOpacity>
+              <Text style={styles.forgotText}>Esqueceu a senha?</Text>
+            </TouchableOpacity>
+          </View>
+
+          <TouchableOpacity style={styles.loginButton}
+          title="Acessar"
+          onPress={() => acessar()}
+          >
+            <Text style={styles.loginText}>Acessar</Text>
+          </TouchableOpacity>
+          
+
+          <TouchableOpacity style={styles.facebookContainer}>
+            <FontAwesome5 name="facebook" size={25} color= "#399fff"/>
+            <Text style={styles.facebookText}>Continue como Marlon Brendon</Text>
+            
+          </TouchableOpacity>
+
+          <View style={styles.divisor}>
+            <View style={styles.divisorLine}></View>
+            <Text style={{marginHorizontal: '3%' }}>OU</Text>
+            <View style={styles.divisorLine}></View>
+          </View>
+
+          <View style={styles.signUpContainer}>
+            <Text style={styles.signUpText}>Não possui uma conta?</Text>
+            <TouchableOpacity>
+              <Text style={styles.signUpButton}>Cadastre-se</Text>
+            </TouchableOpacity>
+          </View>
+
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo:{
+    marginTop: Platform.OS === 'android' ? '10%' : '10%',
+    marginBottom: Platform.OS === 'android' ? '13%' : '15%',
+  },
+  input:{
+    width: '90%',
+    height: 42,
+    backgroundColor: '#F4F3F3',
+    marginBottom: 20,
+    padding: 8,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#E0E0E0',
+  },
+  forgotContainer:{
+    width: '90%',
+    alignItems: 'flex-end'
+  },
+  forgotText:{
+    color: '#399fff',
+  },
+  loginButton:{
+    marginTop: '5%',
+    backgroundColor: '#399fff',
+    width: '90%',
+    height: 42,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  loginText: {
+    color: '#fff',
+    fontSize: 17,
+  },
+  facebookContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: '15%',
+  },
+  facebookText:{
+    color: '#399fff',
+    paddingLeft: 8,
+    fontSize: 15,
+  },
+  divisor:{
+    marginTop: '10%',
+    flexDirection: 'row',
+    width: '90%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  divisorLine:{
+    width: '45%',
+    height: 2,
+    backgroundColor: '#EFEDED',
+    borderRadius: 5,
+  },
+  signUpContainer:{
+    flexDirection: 'row',
+    marginTop: '10%',
+  },
+  signUpText:{
+    color: '#c4c4c4',
+    paddingRight: 5,
+  },
+  signUpButton:{
+    color: '#399fff',
+    fontWeight: 'bold',
+  }
+});
